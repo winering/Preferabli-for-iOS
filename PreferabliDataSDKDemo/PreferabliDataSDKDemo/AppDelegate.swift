e3423669057302e9b4b77cb31a8230cf838339d6
Nicholas Bortolussi <nbortolussi@Nicholass-MacBook-Pro.local>
2023-03-27 13:06:08 -0400

    updated with framework

--- PreferabliDataSDKDemo/PreferabliDataSDKDemo/AppDelegate.swift
+++ PreferabliDataSDKDemo/PreferabliDataSDKDemo/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
                 
-        Preferabli.initialize(client_interface: "YOUR_CLIENT_INTERFACE_HERE", integration_id: 6301, logging_enabled: true)
+        Preferabli.initialize(client_interface: "YOUR_CLIENT_INTERFACE_HERE", integration_id: 630)
         
         return true
     }
